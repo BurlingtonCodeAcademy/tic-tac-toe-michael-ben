dc4036fe409007cb8063dc7b27f2f4f90ac2a425
Ben Chussid <benchussid@Bens-MacBook-Pro.local>
2020-10-11 18:57:54 -0400

    start of reformatting

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ let cell5 = document.getElementById('cell-5')
 let cell6 = document.getElementById('cell-6')
 let cell7 = document.getElementById('cell-7')
 let cell8 = document.getElementById('cell-8')
-
 let playCell = Array.from(document.getElementsByClassName('play-Cell'))
 let startButton = document.getElementById('start')
 let currentStatus = document.getElementById('status')
@@ -44,6 +43,7 @@ let gameWon = false
 
 // Global functions  **********************
 
+// This function references the possible winning combinations and colors them yellow when win conditions are met
 function highlightWinnerRow(index) {
     if (index == 0) {
         cell0.style.backgroundColor = 'yellow'
@@ -86,26 +86,25 @@ function highlightWinnerRow(index) {
         cell2.style.backgroundColor = 'yellow'
     }
 }
-
+// to capitalize player names
 function capitalize(string) {
     return string[0].toUpperCase() + string.slice(1).toLowerCase().trim()
 }
-
+// timer function
 function countUp() {
     interval += 1
     timeNow.textContent = 'Elapsed time: ' + interval + ' seconds'
     console.log(interval)
 }
-
+// disables click event listeners on board
 function turnOffBoard() {
     playCell.forEach((square) => {
         square.removeEventListener('click', gamePlay
         )
     })
 }
-
+// determines how many potential winning rows (9 total) are full - note- this does not evaluate contents of winning rows
 function calcRowsFull() {
-    // Set counter and check how many win-row arrays are full
     rowsFull = 0
     winConditions.forEach((condition) => {
         if (condition.length === 3) {
@@ -113,10 +112,9 @@ function calcRowsFull() {
         }
     })
 }
-
+//checks if winning conditions are satisfied or if game is a draw
 function checkForWin() {
     calcRowsFull()
-
     winConditions.forEach((win) => {
         // for every win-row array, calculate total of contents and length of array
         let total1 = (win.reduce(function (arrayTotal, nextNum) {
@@ -131,7 +129,7 @@ function checkForWin() {
             gameWon = true
             turnOffBoard()
         }
-        // id any soecific win-row array is full and the total is 3 or 6 then check the player code and show who won
+        // id any specific win-row array is full and the total is 3 or 6 then check the player code and show who won
         if (length1 === 3 && total1 % 3 === 0) {
             let wIndex = winConditions.indexOf(win)
             highlightWinnerRow(wIndex)
@@ -150,96 +148,95 @@ function checkForWin() {
         }
     })
 }
-
+// update all winCondition arrays when cell is selected by either player or computer
 function updateArrays(player, cell) {
-
+    // set player code to load in arrray for evaluation in check for win function
     if (player === 'x') {
         arrayValue = 1
     } else {
         arrayValue = 2
     }
-
+    // push player code onto winCondition arrays per each cell
     if (cell === 'cell-0') {
         win1.push(arrayValue)
         win4.push(arrayValue)
         win7.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-1') {
         win1.push(arrayValue)
         win5.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-2') {
         win1.push(arrayValue)
         win6.push(arrayValue)
         win8.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-3') {
         win2.push(arrayValue)
         win4.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-4') {
         win2.push(arrayValue)
         win5.push(arrayValue)
         win7.push(arrayValue)
         win8.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-5') {
         win2.push(arrayValue)
         win6.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-6') {
         win3.push(arrayValue)
         win4.push(arrayValue)
         win8.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-7') {
         win3.push(arrayValue)
         win5.push(arrayValue)
-        checkForWin()
 
     } else if (cell === 'cell-8') {
         win3.push(arrayValue)
         win6.push(arrayValue)
         win7.push(arrayValue)
-        checkForWin()
     }
 }
-
+// this happens when it is computers turn to play
 function computerPlays() {
+    // build a new array from cells with no content
     optionArray = []
     playCell.forEach((option) => {
         if (option.textContent === '') {
             optionArray.push(option)
         }
     })
+    // pick a random number between 0 and the length of the new array
     let compMove = Math.floor(Math.random() * optionArray.length)
+    // click the cell at array index generated above
     optionArray[compMove].click()
 }
-
+// main game play activities
 function gamePlay(event) {
+    // cannot select an occupied cell
     if (event.target.textContent !== '') {
         alert('Please select an Empty Cell')
     } else {
+        // play logic for player X
         if (currentPlayer === 'x') {
             updateArrays(currentPlayer, event.target.id)
+            checkForWin()
             event.target.textContent = playerXToken //drops token
             if (gameWon === false) {
                 currentPlayer = 'o' // change player
                 currentStatus.textContent = capitalize(oPlayer.value) + ": GO!" // displays current player
             }
+            // launch computer turn
             if (oPlayer.value === 'computer') {
                 computerPlays()
             }
-
         } else {
+            //play logic for player O - whether computer or human
             updateArrays(currentPlayer, event.target.id)
+            checkForWin()
             event.target.textContent = playerOToken
             if (gameWon === false) {
                 currentPlayer = 'x'
@@ -248,9 +245,7 @@ function gamePlay(event) {
         }
     }
 }
-
-
-
+// choose player vs player or player vs computer
 function gameChooser() {
     if (vsPlayer.checked === true) {
         gameVersion = 'player'
@@ -260,64 +255,49 @@ function gameChooser() {
         playOn()
     } else {
         alert('Please select a game format.')
-
     }
 }
-
+// auto-load names for palyer vs computer game
 function nameChooser() {
     if (gameVersion === 'computer') {
         oPlayer.value = 'computer'
-        oPlayer.inputMode = 'none' // need to deny input
+        xPlayer.value = 'Player X'
     }
+    timer = setInterval(countUp, 1000)
+    timeNow.style.display = 'block'
 }
 
 // Game play logic ********************************
-
+// hide elements until needed
 nameForm.style.display = 'none'
 timeNow.style.display = 'none'
-
+// enable start button and fire game chooser
 startButton.addEventListener('click', function () {
     gameChooser()
 })
-
+// game play once game version is selected
 function playOn() {
     gameChoice.style.display = 'none'
     startButton.style.display = 'none'
     currentStatus.style.display = 'block'
-
-
+    //submit names and start timer
     currentStatus.textContent = 'Enter player names:'
     nameForm.style.display = 'block'
-    nameChooser()
 
+    if (gameVersion === 'computer') {
+        nameChooser()
+    } else {
+        nameForm.addEventListener('submit', (evt) => {
+            evt.preventDefault()
+            nameForm.style.display = 'none'
+            currentStatus.textContent = capitalize(xPlayer.value) + ': Go!'
+            timer = setInterval(countUp, 1000)
+            timeNow.style.display = 'block'
+        })
+    }
 
-
-
-    nameForm.addEventListener('submit', (evt) => {
-        evt.preventDefault()
-        nameForm.style.display = 'none'
-        currentStatus.textContent = capitalize(xPlayer.value) + ': Go!'
-        timer = setInterval(countUp, 1000)
-        timeNow.style.display = 'block'
-
-    })
-
+    // turn on game board - each cell gets a click listener
     playCell.forEach((move) => {
         move.addEventListener('click', gamePlay)
     })
-
 }
-
-
-// let moves = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8']
-// let moveMap = {
-//     'c0': 0,
-//     'c1': 1,
-//     'c2': 2,
-//     'c3': 3,
-//     'c4': 4,
-//     'c5': 5,
-//     'c6': 6,
-//     'c7': 7,
-//     'c8': 8
-// }
